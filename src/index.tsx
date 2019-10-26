ec24cd9092084e88c94105e268050c5239cd3891
Jean-Baptiste WATENBERG <jeanbaptiste.watenberg@gmail.com>
2019-10-26 21:40:10 +0200

    chore() init project dependency and architecture

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './global/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
